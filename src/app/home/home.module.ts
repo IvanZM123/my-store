9583e5378225fce86179b2565e3dee78237b04b7
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-25 10:43:10 -0600

    ItemCard component was added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,11 +6,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     CarouselComponent,
+    ItemCardComponent,
   ],
   imports: [
     CommonModule,
--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  items = [
+  carousel = [
     {
       title: "Visualiza nuestros clientes",
       subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
@@ -24,6 +24,26 @@ export class HomeComponent {
       subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       image: "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_637/v1621025103/assets/58/fed8fc-73e7-428e-a68c-802887085104/original/hero_manage_orders_3x2_2x.png",
       color: "skin"
+    }
+  ];
+  items = [
+    {
+      title: "Lista de productos",
+      subtitle: "mas de 10 productos",
+      image: "https://image.flaticon.com/icons/png/512/2250/2250295.png",
+      color: "brown"
+    },
+    {
+      title: "Lista de clientes",
+      subtitle: "mas de 20 clientes",
+      image: "https://image.flaticon.com/icons/png/512/4149/4149883.png",
+      color: "green"
+    },
+    {
+      title: "Lista de ordenes",
+      subtitle: "mas de 25 ordenes",
+      image: "https://image.flaticon.com/icons/png/512/5439/5439012.png",
+      color: "yellow"
     }
   ];
 }
