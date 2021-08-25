e05fbb5a5e95a8d843ba61854c7f272bc5bac711
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-25 16:54:44 -0600

    ProductService was added

---
+++ src/app/core/services/products/product.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  picture: string;
+  categoryId: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private url: string = `${ environment.url }/products`;
+
+  constructor(private http: HttpClient) {}
+
+  list(): Observable<Array<Product>> {
+    return this.http.get<Array<Product>>(`${ this.url }?_expand=categories`);
+  }
+}
--- src/app/products/pages/list-products-page/list-products-page.component.ts
+++ src/app/products/pages/list-products-page/list-products-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from 'src/app/core/services/products/product.service';
+
 @Component({
   selector: 'app-list-products-page',
   templateUrl: './list-products-page.component.html',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService.list().subscribe(console.log);
   }
-
 }
