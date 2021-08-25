0301eff8897704958758f841ac64e46d9e47b6a2
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-24 23:01:14 -0600

    CarouselComponent was added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+
 import { HomeComponent } from './pages/home/home.component';
 
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule
   ]
 })
-export class HomeModule { }
+export class HomeModule {}
