8d52d7109277419c525493e63eb4012773355be0
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-26 22:24:14 -0600

    The order page was stylized

---
+++ src/app/core/services/orders/order.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { Client } from '../clients/clients.service';
+import { Product } from '../products/product.service';
+
+export interface Order {
+  id: string;
+  quantity: number;
+  productsId: number;
+  clientsId: number;
+  products: Product;
+  clients: Client;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  private readonly url: string = `${ environment.url }/orders`;
+
+  constructor(private http: HttpClient) {}
+
+  list(): Observable<Array<Order>> {
+    const url: string = `${ this.url }?_expand=clients&_expand=products`;
+    return this.http.get<Array<Order>>(url);
+  }
+}
--- src/app/orders/pages/orderlist-page/orderlist-page.component.ts
+++ src/app/orders/pages/orderlist-page/orderlist-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Order, OrderService } from 'src/app/core/services/orders/order.service';
+
 @Component({
   selector: 'app-orderlist-page',
   templateUrl: './orderlist-page.component.html',
   styleUrls: ['./orderlist-page.component.css']
 })
 export class OrderlistPageComponent implements OnInit {
+  orders: Array<Order> = [];
 
-  constructor() { }
+  constructor(private orderService: OrderService) {}
 
   ngOnInit(): void {
+    this.orderService.list().subscribe(
+      orders => this.orders = orders
+    );
   }
-
 }
