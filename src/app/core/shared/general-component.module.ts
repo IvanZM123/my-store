90abb0f43b04fb5770654c871d18366c163b682f
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-28 13:35:52 -0600

    Updated GeneralComponentModule file

--- src/app/core/shared/general-component.module.ts
+++ src/app/core/shared/general-component.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AngularMaterial } from '../material/angular-material';
 
 import { OrderStickerComponent } from '../components/order-sticker/order-sticker.component';
 import { ClientTableComponent } from '../components/client-table/client-table.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
