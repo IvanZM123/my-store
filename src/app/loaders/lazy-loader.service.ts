78e779ee63ae174d5cce66e34d38f43093d392ee
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-28 09:50:36 -0600

    The products module was moved to the modules directory

--- src/app/loaders/lazy-loader.service.ts
+++ src/app/loaders/lazy-loader.service.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: "products",
-    loadChildren: () => import("../products/products.module").then(m => m.ProductsModule)
+    loadChildren: () => import("../modules/products/products.module").then(m => m.ProductsModule)
   },
   {
     path: "orders",
--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ClientsRoutingModule } from './clients-routing.module';
-
 import { GeneralComponentModule } from '../../core/shared/general-component.module';
 
 import { ClientListPageComponent } from './pages/client-list-page/client-list-page.component';
 import { ClientDetailsPageComponent } from './pages/client-details-page/client-details-page.component';
 import { AddClientPageComponent } from './pages/add-client-page/add-client-page.component';
+
 import { ClientCardComponent } from './components/client-card/client-card.component';
 
 @NgModule({
