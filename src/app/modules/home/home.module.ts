680c6a4bd1cd4396318deaf7659e6e7e0bee786f
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-28 09:56:41 -0600

    The carousel component was removed

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,13 +6,11 @@ import { GeneralComponentModule } from '../../core/shared/general-component.modu
 
 import { HomeComponent } from './pages/home/home.component';
 
-import { CarouselComponent } from './components/carousel/carousel.component';
 import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    CarouselComponent,
     ItemCardComponent,
   ],
   imports: [
