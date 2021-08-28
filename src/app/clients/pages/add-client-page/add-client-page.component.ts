bc4a36df7a104e40585b4b1e1fdf8b772f39f4e4
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-08-27 19:14:24 -0600

    Added validation to customer form

--- src/app/clients/pages/add-client-page/add-client-page.component.ts
+++ src/app/clients/pages/add-client-page/add-client-page.component.ts
@@ -5,6 +5,7 @@ import { Store as NgrxStore } from "@ngrx/store";
 import { Store } from "src/app/core/store/index";
 
 import { StartClientCreate } from 'src/app/core/store/clients/client.actions';
+import { rules } from 'src/app/core/config/rules.config';
 
 @Component({
   selector: 'app-add-client-page',
@@ -22,15 +23,15 @@ export class AddClientPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.personalDataForm = this.formBuilder.group({
-      firstName: new FormControl("", [Validators.required]),
-      lastName: new FormControl("", [Validators.required]),
-      avatar: new FormControl("", [Validators.required])
+      firstName: new FormControl("", [Validators.required, Validators.pattern(rules.name)]),
+      lastName: new FormControl("", [Validators.required, Validators.pattern(rules.name)]),
+      avatar: new FormControl("", [Validators.required, Validators.pattern(rules.url)])  
     });
 
     this.additionalDataForm = this.formBuilder.group({
-      phone: new FormControl("", [Validators.required]),
-      NIT: new FormControl("", [Validators.required]),
-      email: new FormControl("", [Validators.email])
+      phone: new FormControl("", [Validators.required, Validators.pattern(rules.phone)]),
+      NIT: new FormControl("", [Validators.required, Validators.pattern(rules.NIT)]),
+      email: new FormControl("", [Validators.email, Validators.email])
     });
   }
 
@@ -44,5 +45,47 @@ export class AddClientPageComponent implements OnInit {
       payload: { ...personalData, ...additionalData }
     });
     this.store.dispatch(action);
+  }
+
+  getFirstNameError() {
+    if (this.personalDataForm.get("firstName")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "El nombre es invalido.";
+  }
+
+  getLastNameError() {
+    if (this.personalDataForm.get("lastName")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "El nombre es invalido.";
+  }
+
+  getUrlError() {
+    if (this.personalDataForm.get("avatar")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "La URL es invalida";
+  }
+
+  getPhoneError() {
+    if (this.personalDataForm.get("phone")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "El numero de telefono es invalido";
+  }
+
+  getNITError() {
+    if (this.personalDataForm.get("NIT")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "El NIT es invalido. Siga la siguiente estructura 0000-0000-0000-0";
+  }
+
+  getEmailError() {
+    if (this.personalDataForm.get("email")?.hasError("required")) {
+      return "Este campo es requerido";
+    }
+    return "El email es invalido. Siga la siguiente estrutura abc@gmail.com";
   }
 }
